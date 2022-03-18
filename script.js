9ebef4f619eaaa52ce70848f94e14e18a19278a7
anhtay <tayphongtran@gmail.com>
2022-03-19 00:54:14 +0700

    gà không chịu được

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 // câu1
-let a = +prompt('nhập hệ số a vào')
-let b = +prompt('nhập hệ số b vào')
-
-function ptbn() {
-    let c = -b/ a
-    if (a==0&& b!=0){
-        alert('pt vô nghiệm')
-
-    }
-    if (a==0&& b==0){
-        alert('pt vô số nghiệm')
-    }
-    return document.writeln('nghiệm ptbn = ' + c)
-}
-
-ptbn(a, b);
+// let a = +prompt('nhập hệ số a vào')
+// let b = +prompt('nhập hệ số b vào')
+//
+// function ptbn() {
+//     let c = -b/ a
+//     if (a==0&& b!=0){
+//         alert('pt vô nghiệm')
+//
+//     }
+//     if (a==0&& b==0){
+//         alert('pt vô số nghiệm')
+//     }
+//     return document.writeln('nghiệm ptbn = ' + c)
+// }
+//
+// ptbn(a, b);
 
 // cau 2;
-// let arr = [11,2,23,4,5,6,7,15];
-// let b = +prompt('so phan tu can tim la');
+
 //
 // function indexoff(){
-//     for (let i = 0; i < arr.length; i++) {
+//     let count = 0
+//     for (let i = 0; count < arr.length; i++) {
 //         if (b == arr[i]){
 //             document.writeln('giá trị phần tử' + b)
 //             document.writeln('vi tri' + arr.indexOf(b))
-//             break
-//         }else {
-//             document.writeln(b + ' khong co')
+//             count++
 //             break
 //         }
 //     }
-//
 // }
-// indexoff();
+// indexoff(arr,b);
 
+// let arr = [11, 2, 23, 4, -5, 6, 7, 15];
+// let b = +prompt('so phan tu can tim la nếu đúng thì in ra còn sai thì  in ra -1: ');
+//
+//
+// function indexoff() {
+//     let count = 0
+//     for (let i = 0; count < arr.length; i +=1) {
+//         count += arr[i]
+//         if (count) {
+//             document.writeln('vi tri =' + arr.indexOf(b) + ' ' +'giá trị'+  ' '+ b)
+//             break
+//         }
+//     }
+// }
+// indexoff(b,arr)
 
 // cau3 Viết chương trình nhập vào một chuỗi bất kỳ. Gọi hàm tìm số ký tự nguyên
 // âm vừa xây dựng ở trên
